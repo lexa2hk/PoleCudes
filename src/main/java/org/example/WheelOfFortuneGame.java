11406b66b87734bb952295ffc9fea4ad320164b9
Alex Smolnikov <alesha.smolnikoff@gmail.com>
2023-10-08 17:58:47 +0300

    Smolnikov TDD WheelOfFortune added code

--- src/main/java/org/example/WheelOfFortuneGame.java
+++ src/main/java/org/example/WheelOfFortuneGame.java
@@ -17,17 +17,41 @@ public class WheelOfFortuneGame {
     }
 
     public void initializeRound() {
+        List<Player> players = new ArrayList<>();
+        players.add(new Player("Player 1"));
+        players.add(new Player("Player 2"));
+        players.add(new Player("Player 3"));
+
+        List<WheelSector> wheel = new ArrayList<>();
+        wheel.add(new WheelSector("Prize", 500, false));
+        wheel.add(new WheelSector("Plus", 100, false));
+        wheel.add(new WheelSector("Key", 200, false));
+        wheel.add(new WheelSector("Bankrupt", 0, false));
+        wheel.add(new WheelSector("Zero", 0, false));
+        wheel.add(new WheelSector("Double", 0, false));
+
+        String theme = "Example Theme";
+
+        currentRound = new Round(players, theme, wheel);
 
     }
 
     //Smolnikov
     public WheelSector spinWheel() {
+        Random random = new Random();
+        int randomIndex = random.nextInt(currentRound.getWheel().size());
+        return currentRound.getWheel().get(randomIndex);
 
-        return null;
     }
 
     public Player determineWinner() {
-        return null;
+        Player winner = currentRound.getPlayers().get(0);
+        for (Player player : currentRound.getPlayers()) {
+            if (player.getScore() > winner.getScore()) {
+                winner = player;
+            }
+        }
+        return winner;
     }
 
     private void handleSectorAction(WheelSector sector, Player currentPlayer) {
